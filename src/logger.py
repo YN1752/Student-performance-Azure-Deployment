1a836026b3eb87d1eec8e56e8b5d45c573b3668c
Yash Nayak <ynayak1752@gmail.com>
2025-09-03 20:37:38 -0400

    Data Ingestion

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from datetime import datetime
+import sys
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y')}.log"
 logs_path=os.path.join(os.getcwd(),"logs")
@@ -9,9 +10,12 @@ os.makedirs(logs_path,exist_ok=True)
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
+    handlers= [
+        logging.FileHandler(LOG_FILE_PATH),
+        logging.StreamHandler(sys.stdout)
+    ]
 )
 
 if __name__=="__main__":
