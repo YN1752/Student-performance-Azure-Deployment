981659ba1a1d153e778395d9c6bf9b0f9dc2e382
Yash Nayak <ynayak1752@gmail.com>
2025-09-04 10:26:56 -0400

    Model Trainer

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -1,5 +1,6 @@
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 if __name__ == "__main__":
     obj = DataIngestion()
@@ -7,3 +8,6 @@ if __name__ == "__main__":
 
     data_transformation = DataTransformation()
     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_path, test_path)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
